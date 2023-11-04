a5bb07689040d59b3fee216a3f661b0b9fe16751
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2023-11-03 21:33:48 -0500

    Creates pdf with incorrect guesses

--- mnist.py
+++ mnist.py
@@ -1,8 +1,11 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 (ds_train, ds_test), ds_info = tfds.load(
-    'mnist', 
+    'mnist',
     split=['train', 'test'],
     shuffle_files=True,
     as_supervised=True,
@@ -27,7 +30,7 @@ ds_test = ds_test.cache()
 ds_test = ds_test.prefetch(tf.data.AUTOTUNE)
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)), 
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(10)
 ])
@@ -43,3 +46,26 @@ model.fit(
     epochs=6,
     validation_data=ds_test,
 )
+
+predictions = model.predict(ds_test.unbatch().batch(ds_info.splits['test'].num_examples))
+predicted_labels = np.argmax(predictions, axis=1)
+
+true_labels, test_images = [], []
+
+for image, label in tfds.as_numpy(ds_test.unbatch()):
+    true_labels.append(label)
+    test_images.append(image)
+
+true_labels = np.array(true_labels)
+test_images = np.array(test_images)
+
+mismatches = np.where(predicted_labels != true_labels)[0]
+
+with PdfPages('misclassified.pdf') as pdf:
+    for idx in mismatches:
+        plt.figure(figsize=(8,8))
+        plt.imshow(test_images[idx].squeeze(), cmap='gray')
+        plt.title(f"Actual: {true_labels[idx]}, Predicted: {predicted_labels[idx]}")
+        plt.axis('off')
+        pdf.savefig()
+    plt.close()
